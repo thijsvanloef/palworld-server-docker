9d03e933e0466e07be62257ce7e47b10c62f8299
Thijs van Loef <thijsvanloef@gmail.com>
2024-01-25 18:23:14 +0100

    add quotes to prevent word splitting

--- scripts/init.sh
+++ scripts/init.sh
@@ -18,11 +18,11 @@ if [ "${UPDATE_ON_BOOT}" = true ]; then
 fi
 
 term_handler() {
-    if [ ${RCON_ENABLED} = true ]; then
+    if [ "${RCON_ENABLED}" = true ]; then
         rcon-cli save
         rcon-cli shutdown 1
     else # Does not save
-        kill -SIGTERM $(pidof PalServer-Linux-Test)
+        kill -SIGTERM "$(pidof PalServer-Linux-Test)"
     fi
     tail --pid=$killpid -f 2>/dev/null
 }
