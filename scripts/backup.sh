f6a3b982b20b70df42c4d5223701a3d1994488ee
Carlos M. Martinez <Dashboy1998@gmail.com>
2024-01-28 16:07:32 -0500

    Removed changes in backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,13 +5,7 @@ if [ "${RCON_ENABLED}" = true ]; then
 fi
 
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-DESTINATION_PATH="/palworld/backups"
-FILE_PATH="${DESTINATION_PATH}/backup_palworld_${DATE}.tar.gz"
-
-if [ ! -f ${FILE_PATH} ]; then
-    printf "\e[0;32m***** CREATING BACKUPS FOLDER *****\e[0m\n"
-    mkdir -p "${DESTINATION_PATH}"
-fi
+FILE_PATH="/palworld/backups/palworld-save-${DATE}.tar.gz"
 
 cd /palworld/Pal/ || exit
 
@@ -23,13 +17,4 @@ fi
 
 echo "backup created at $FILE_PATH"
 
-if [[ -n "${DAYS_TO_KEEP}" && "${DAYS_TO_KEEP}" =~ ^[0-9]+$ ]]; then
-    echo "DAYS_TO_KEEP=${DAYS_TO_KEEP}"
-    if [[ "${DAYS_TO_KEEP}" -gt 0 ]]; then
-        find "${DESTINATION_PATH}" -type f -mtime +"${DAYS_TO_KEEP}" -exec rm {} \;
-    else
-        echo "DAYS_TO_KEEP is zero. No files will be removed."
-    fi
-fi
-
-
+# Adding auto delete here
