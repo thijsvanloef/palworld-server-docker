8715740ed96d77b28b1d6d4b9a1dfa3c0df4e5eb
luatan <simonschmid@hispeed.ch>
2024-01-29 21:29:14 +0100

    fix linting

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,17 +2,17 @@
 
 if [ "${UPDATE_ON_BOOT}" = false ]; then
     echo "Update on Boot needs to be enabled for auto updating"
-    exit
+    exit 1
 fi
 
-CURRENTBUILD=$(cat /palworld/steamapps/appmanifest_2394010.acf | awk '/buildid/{ print $2 }')
+CURRENTBUILD=$(awk '/buildid/{ print $2 }' < palworld/steamapps/appmanifest_2394010.acf)
 TARGETBUILD=$(curl https://api.steamcmd.net/v1/info/2394010 --silent | grep -P '"public": {"buildid": "\d+"' -o | sed -r 's/.*("[0-9]+")$/\1/')
 
 if [ "$CURRENTBUILD" != "$TARGETBUILD" ]; then
     echo "New Build was found. Updating the server from $CURRENTBUILD to $TARGETBUILD."
     if [ "${RCON_ENABLED}" = true ]; then
         rm /palworld/steamapps/appmanifest_2394010.acf
-        rcon-cli -c /home/steam/server/rcon.yaml "broadcast "The_Server_will_update_in_${WARN_MINUTES}_Minutes""
+        rcon-cli -c /home/steam/server/rcon.yaml "broadcast The_Server_will_update_in_${WARN_MINUTES}_Minutes"
         sleep "${WARN_MINUTES}m"
         rcon-cli -c /home/steam/server/rcon.yaml save
         rcon-cli -c /home/steam/server/rcon.yaml "shutdown 1"
