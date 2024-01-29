bdfacf03499f58eae235054cecf38a16965dc98a
luatan <simonschmid@hispeed.ch>
2024-01-29 19:05:47 +0100

    add update check script

---
+++ scripts/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "${UPDATE_ON_BOOT}" = false ]; then
+    echo "Update on Boot needs to be enabled for auto updating"
+    exit
+fi
+
+CURRENTBUILD=$(cat /palworld/steamapps/appmanifest_2394010.acf | awk '/buildid/{ print $2 }')
+TARGETBUILD=$(curl https://api.steamcmd.net/v1/info/2394010 --silent | grep -P '"public": {"buildid": "\d+"' -o | sed -r 's/.*("[0-9]+")$/\1/')
+
+if [ "$CURRENTBUILD" != "$TARGETBUILD" ]; then
+    echo "New Build was found. Updating the server from $CURRENTBUILD to $TARGETBUILD."
+    if [ "${RCON_ENABLED}" = true ]; then
+        rm /palworld/steamapps/appmanifest_2394010.acf
+        rcon-cli -c /home/steam/server/rcon.yaml "broadcast "The_Server_will_update_in_${WARN_MINUTES}_Minutes""
+        sleep "${WARN_MINUTES}m"
+        rcon-cli -c /home/steam/server/rcon.yaml save
+        rcon-cli -c /home/steam/server/rcon.yaml "shutdown 1"
+    else
+        kill -SIGTERM "$(pidof PalServer-Linux-Test)"
+    fi
+else
+    echo "The Server is up to date!"
+fi
