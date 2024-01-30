4fb73aabba52d43de6f8446f327f88e19d0b89c6
luatan <simonschmid@hispeed.ch>
2024-01-30 10:00:13 +0100

    check server response and react accordingly

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,13 +2,27 @@
 
 if [ "${UPDATE_ON_BOOT}" = false ]; then
     echo "Update on Boot needs to be enabled for auto updating"
+    exit 0
+fi
+
+temp_file=$(mktemp)
+http_code=$(curl https://api.steamcmd.net/v1/info/2394010 --output $temp_file --silent --location --write-out "%{http_code}")
+
+CURRENTBUILD=$(awk '/buildid/{ print $2 }' < /palworld/steamapps/appmanifest_2394010.acf)
+TARGETBUILD=$(grep -P '"public": {"buildid": "\d+"' -o <"$temp_file" | sed -r 's/.*("[0-9]+")$/\1/')
+rm $temp_file
+
+if [ "$http_code" -ne 200 ]; then
+    echo "There was a problem reaching the Steam api. Unable to check for updates!"
     exit 1
 fi
 
-CURRENTBUILD=$(awk '/buildid/{ print $2 }' < /palworld/steamapps/appmanifest_2394010.acf)
-TARGETBUILD=$(curl https://api.steamcmd.net/v1/info/2394010 --silent | grep -P '"public": {"buildid": "\d+"' -o | sed -r 's/.*("[0-9]+")$/\1/')
+if [ -z "$TARGETBUILD" ]; then
+    echo "The server response does not contain the expected BuildID. Unable to check for updates!"
+    exit 1
+fi
 
-if [ -n "$TARGETBUILD" ] && [ "$CURRENTBUILD" != "$TARGETBUILD" ]; then
+if [ "$CURRENTBUILD" != "$TARGETBUILD" ]; then
     echo "New Build was found. Updating the server from $CURRENTBUILD to $TARGETBUILD."
     if [ "${RCON_ENABLED}" = true ]; then
         rm /palworld/steamapps/appmanifest_2394010.acf
