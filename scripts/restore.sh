5f16b7734a7bae4191f6b51b4b1c0d74eca3f848
seoseonyu <dev@seoseonyu.com>
2024-02-01 00:17:43 +0900

    Remove unnecessary functions

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -11,20 +11,6 @@ term_error_handler() {
     exit 1
 }
 
-term_handler() {
-  trap 'term_error_handler' ERR
-
-  printf "\e[0;32m*****SHUTDOWN SERVER*****\e[0m\n"
-  if [ "${RCON_ENABLED}" = true ]; then
-      rcon-cli -c /home/steam/server/rcon.yaml save
-      rcon-cli -c /home/steam/server/rcon.yaml "shutdown 1"
-  else
-      echo "RCON is not enabled. Please enable RCON to use this feature."
-  fi
-  printf "\e[0;32mShutdown complete.\e[0m\n"
-  trap - ERR
-}
-
 restore_error_handler() {
     printf "\033[0;31mAn error occurred during restore.\033[0m\n"
     if [ -d "./tmp_save/Saved" ]; then
@@ -40,54 +26,6 @@ restore_error_handler() {
     rm -rf "./tmp_save"
 
     exit 1
-}
-
-start_restore() {
-  trap 'restore_error_handler' ERR
-  # Shutdown server
-  term_handler
-  printf "\e[0;32m*****START RESTORE*****\e[0m\n"
-
-  # Recheck the backup file
-  if [ -f "$BACKUP_FILE" ]; then
-    ls
-    # Copy the save file before restore
-      if [ -d "$RESTORE_PATH/Saved" ]; then
-      echo "Saves the current state before the restore proceeds."
-      rm -rf "./tmp_save"
-      mkdir -p "./tmp_save"
-      \cp -rf "$RESTORE_PATH/Saved" "./tmp_save/Saved"
-
-      while [ ! -d "./tmp_save/Saved" ]; do
-        sleep 1
-      done
-
-      printf "\e[0;32mSave complete.\e[0m\n"
-    fi
-    
-    # Create tmp directory
-    rm -rf "./tmp"
-    mkdir -p "./tmp"
-     
-    # Decompress the backup file in tmp directory
-    tar -zxvf "$BACKUP_FILE" -C "./tmp"
-
-    # Move the backup file to the restore directory
-    \cp -rf -f "./tmp/Saved/" "$RESTORE_PATH"
-
-    # Remove tmp directory
-    rm -rf "./tmp"
-    rm -rf "./tmp_save"
-
-    printf "\e[0;32mRestore complete!!!! Please restart the Docker container\e[0m\n"
-
-    exit 0
-  else 
-    echo "The selected backup file does not exist."
-    exit 1
-  fi
-
-  trap - ERR
 }
 
 if [ "${RCON_ENABLED}" != true ]; then
@@ -111,7 +49,62 @@ if [ -f "$BACKUP_FILE" ]; then
   read -rp "When you run it, the server will be stopped and the recovery will proceed. (y/n): " RUN_ANSWER
   if [[ $RUN_ANSWER == "y" ]] || [[ $RUN_ANSWER == "Y" ]]; then
     printf "\e[0;32m*****STARTING PROCESS*****\e[0m\n"
-      start_restore
+    # Shutdown server
+    trap 'term_error_handler' ERR
+
+    printf "\e[0;32m*****SHUTDOWN SERVER*****\e[0m\n"
+    if [ "${RCON_ENABLED}" = true ]; then
+        rcon-cli -c /home/steam/server/rcon.yaml save
+        rcon-cli -c /home/steam/server/rcon.yaml "shutdown 1"
+    else
+        echo "RCON is not enabled. Please enable RCON to use this feature."
+    fi
+    printf "\e[0;32mShutdown complete.\e[0m\n"
+    
+    trap - ERR
+
+    trap 'restore_error_handler' ERR
+    
+    printf "\e[0;32m*****START RESTORE*****\e[0m\n"
+
+    # Recheck the backup file
+    if [ -f "$BACKUP_FILE" ]; then
+      # Copy the save file before restore
+        if [ -d "$RESTORE_PATH/Saved" ]; then
+        echo "Saves the current state before the restore proceeds."
+        rm -rf "./tmp_save"
+        mkdir -p "./tmp_save"
+        \cp -rf "$RESTORE_PATH/Saved" "./tmp_save/Saved"
+
+        while [ ! -d "./tmp_save/Saved" ]; do
+          sleep 1
+        done
+
+        printf "\e[0;32mSave complete.\e[0m\n"
+      fi
+      
+      # Create tmp directory
+      rm -rf "./tmp"
+      mkdir -p "./tmp"
+      
+      # Decompress the backup file in tmp directory
+      tar -zxvf "$BACKUP_FILE" -C "./tmp"
+
+      # Move the backup file to the restore directory
+      \cp -rf -f "./tmp/Saved/" "$RESTORE_PATH"
+
+      # Remove tmp directory
+      rm -rf "./tmp"
+      rm -rf "./tmp_save"
+
+      printf "\e[0;32mRestore complete!!!! Please restart the Docker container\e[0m\n"
+
+      exit 0
+    else 
+      echo "The selected backup file does not exist."
+      exit 1
+    fi
+    trap - ERR
   else
       echo "Abort the recovery."
       exit 1
