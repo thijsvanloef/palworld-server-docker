5fbcc005360e06421dafbe0e6ac31ab787d42de2
seoseonyu <dev@seoseonyu.com>
2024-02-01 23:18:10 +0900

    Add Wait for restore to complete

--- scripts/init.sh
+++ scripts/init.sh
@@ -36,3 +36,11 @@ if [ "${#backup_pids[@]}" -ne 0 ]; then
         tail --pid="$pid" -f 2>/dev/null
     done
 fi
+
+mapfile -t restore_pids < <(pgrep restore)
+if [ "${#restore_pids[@]}" -ne 0 ]; then
+    echo "Waiting for restore to finish"
+    for pid in "${restore_pids[@]}"; do
+        tail --pid="$pid" -f 2>/dev/null
+    done
+fi
