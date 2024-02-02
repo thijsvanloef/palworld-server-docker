b0c5dc2497c5cbb0fb64ef8ebd0b834ca7e71079
luatan <simonschmid@hispeed.ch>
2024-02-02 20:10:41 +0100

    add pause script to freeze the server

---
+++ scripts/pause.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+PID=$(pidof PalServer-Linux-Test)
+PID_STATE=$(grep State /proc/$PID/status | cut -d '(' -f2 | cut -d ')' -f1)
+LOCKFILE=/tmp/port_listener.lock
+
+if [ -f "$LOCKFILE" ]; then
+    exit 0
+fi
+
+if [ "${RCON_ENABLED}" = true ]; then
+    PLAYERLIST=$(rcon-cli -c /home/steam/server/rcon.yaml "ShowPlayers")
+
+    if [ $(echo -n "$PLAYERLIST" | wc -l) -gt 0 ]; then
+        exit 0
+    fi
+    rcon-cli -c /home/steam/server/rcon.yaml save  
+fi
+
+monitor_traffic() {
+    touch "$LOCKFILE"
+    cleanup() {
+        rm -f "$LOCKFILE"
+    }
+    while true; do
+        packet_capture=$(tcpdump -n -c 1 -i any port $1 2>/dev/null)
+        if [ -n "$packet_capture" ]; then
+            echo "Connection attempt detected resuming the server" >> /proc/1/fd/1
+            kill -CONT "$2"
+            break
+        fi
+        sleep 1
+    done
+    trap 'cleanup' EXIT
+}
+
+if [ "$PID_STATE" != "stopped" ]; then
+    kill -STOP $PID
+    echo "Server paused" >> /proc/1/fd/1
+    monitor_traffic $PORT $PID &
+fi
