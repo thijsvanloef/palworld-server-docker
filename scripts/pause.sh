95fc7e723e5e0ed253a18c07e27f1ea475de38e2
luatan <simonschmid@hispeed.ch>
2024-02-02 20:57:42 +0100

    added quotes

--- scripts/pause.sh
+++ scripts/pause.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PID=$(pidof PalServer-Linux-Test)
-PID_STATE=$(grep State /proc/$PID/status | cut -d '(' -f2 | cut -d ')' -f1)
+PID_STATE=$(grep State "/proc/$PID/status" | cut -d '(' -f2 | cut -d ')' -f1)
 LOCKFILE=/tmp/port_listener.lock
 
 if [ -f "$LOCKFILE" ]; then
@@ -10,7 +10,7 @@ fi
 if [ "${RCON_ENABLED}" = true ]; then
     PLAYERLIST=$(rcon-cli -c /home/steam/server/rcon.yaml "ShowPlayers")
 
-    if [ $(echo -n "$PLAYERLIST" | wc -l) -gt 0 ]; then
+    if [ "$(echo -n "$PLAYERLIST" | wc -l)" -gt 0 ]; then
         exit 0
     fi
     rcon-cli -c /home/steam/server/rcon.yaml save  
@@ -22,7 +22,7 @@ monitor_traffic() {
         rm -f "$LOCKFILE"
     }
     while true; do
-        packet_capture=$(tcpdump -n -c 1 -i any port $1 2>/dev/null)
+        packet_capture=$(tcpdump -n -c 1 -i any port "$1" 2>/dev/null)
         if [ -n "$packet_capture" ]; then
             echo "Connection attempt detected resuming the server" >> /proc/1/fd/1
             kill -CONT "$2"
@@ -34,7 +34,7 @@ monitor_traffic() {
 }
 
 if [ "$PID_STATE" != "stopped" ]; then
-    kill -STOP $PID
+    kill -STOP "$PID"
     echo "Server paused" >> /proc/1/fd/1
-    monitor_traffic $PORT $PID &
+    monitor_traffic "$PORT" "$PID" &
 fi
