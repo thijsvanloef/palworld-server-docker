801180dc8d80fb4d5bafb73cec7fad5a851e2827
Carlos M. Martinez <Dashboy1998@gmail.com>
2024-02-08 13:02:58 -0500

    Added descriptions for functions

--- scripts/helper_functions.sh
+++ scripts/helper_functions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This file contains functions which can be used in multiple scripts
 
+# Checks if a given path is a directory
+# Returns 0 if the path is a directory
+# Returns 1 if the path is not a directory or does not exists and produces an output message
 dirExists() {
     local path="$1"
     local return_val=0
@@ -11,6 +14,9 @@ dirExists() {
     return "$return_val"
 }
 
+# Checks if a given path is a regular file
+# Returns 0 if the path is a regular file
+# Returns 1 if the path is not a regular file or does not exists and produces an output message
 fileExists() {
     local path="$1"
     local return_val=0
@@ -21,6 +27,9 @@ fileExists() {
     return "$return_val"
 }
 
+# Checks if a given path exists and is readable
+# Returns 0 if the path exists and is readable
+# Returns 1 if the path is not readable or does not exists and produces an output message
 isReadable() {
     local path="$1"
     local return_val=0
@@ -31,6 +40,9 @@ isReadable() {
     return "$return_val"
 }
 
+# Checks if a given path is writable
+# Returns 0 if the path is writable
+# Returns 1 if the path is not writable or does not exists and produces an output message
 isWritable() {
     local path="$1"
     local return_val=0
@@ -41,6 +53,9 @@ isWritable() {
     return "$return_val"
 }
 
+# Checks if a given path is executable
+# Returns 0 if the path is executable
+# Returns 1 if the path is not executable or does not exists and produces an output message
 isExecutable() {
     local path="$1"
     local return_val=0
