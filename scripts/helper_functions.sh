a3d6c50a948cf41435c86def3291fe3ab3b2d49e
Carlos M. Martinez <Dashboy1998@gmail.com>
2024-02-08 12:16:27 -0500

    Moved helper functions to own script

---
+++ scripts/helper_functions.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# This file contains functions which can be used in multiple scripts
+
+dirExists() {
+    local path="$1"
+    local return_val=0
+    if ! [ -d "${path}" ]; then
+        echo "${path} does not exist."
+        return_val=1
+    fi
+    return "$return_val"
+}
+
+fileExists() {
+    local path="$1"
+    local return_val=0
+    if ! [ -f "${path}" ]; then
+        echo "${path} does not exist."
+        return_val=1
+    fi
+    echo "Return code: $return_val"
+    return "$return_val"
+}
+
+isReadable() {
+    local path="$1"
+    local return_val=0
+    if ! [ -e "${path}" ]; then
+        echo "${path} is not readable."
+        return_val=1
+    fi
+    return "$return_val"
+}
+
+isWritable() {
+    local path="$1"
+    local return_val=0
+    if ! [ -w "${path}" ]; then
+        echo "${path} is not writable."
+        return_val=1
+    fi
+    return "$return_val"
+}
+
+isExecutable() {
+    local path="$1"
+    local return_val=0
+    if ! [ -x "${path}" ]; then
+        echo "${path} is not executable."
+        return_val=1
+    fi
+    return "$return_val"
+}
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,54 +1,5 @@
 #!/bin/bash
-
-dirExists() {
-    local path="$1"
-    local return_val=0
-    if ! [ -d "${path}" ]; then
-        echo "${path} does not exist."
-        return_val=1
-    fi
-    return "$return_val"
-}
-
-fileExists() {
-    local path="$1"
-    local return_val=0
-    if ! [ -f "${path}" ]; then
-        echo "${path} does not exist."
-        return_val=1
-    fi
-    return "$return_val"
-}
-
-isReadable() {
-    local path="$1"
-    local return_val=0
-    if ! [ -e "${path}" ]; then
-        echo "${path} is not readable."
-        return_val=1
-    fi
-    return "$return_val"
-}
-
-isWritable() {
-    local path="$1"
-    local return_val=0
-    if ! [ -w "${path}" ]; then
-        echo "${path} is not writable."
-        return_val=1
-    fi
-    return "$return_val"
-}
-
-isExecutable() {
-    local path="$1"
-    local return_val=0
-    if ! [ -x "${path}" ]; then
-        echo "${path} is not executable."
-        return_val=1
-    fi
-    return "$return_val"
-}
+source /home/steam/scripts/helper_functions.sh
 
 dirExists "/palworld" || exit
 isWritable "/palworld" || exit
