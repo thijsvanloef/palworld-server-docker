32c358da55482d55ad3a835ccf4c69979ad60b55
Patrick <Patrick@networkglitch.com>
2024-02-10 18:18:31 -0500

    Implemented Logging and discord functions throughout script.

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+source "/home/steam/server/helper_functions.sh"
 
 if [[ ! "${PUID}" -eq 0 ]] && [[ ! "${PGID}" -eq 0 ]]; then
-    printf "\e[0;32m*****EXECUTING USERMOD*****\e[0m\n"
+    LogAction "EXECUTING USERMOD"
     usermod -o -u "${PUID}" steam
     groupmod -o -g "${PGID}" steam
 else
-    printf "\033[31m%s\n" "Running as root is not supported, please fix your PUID and PGID!"
+    LogError "Running as root is not supported, please fix your PUID and PGID!"
     exit 1
 fi
 
@@ -14,9 +15,7 @@ chown -R steam:steam /palworld /home/steam/
 
 # shellcheck disable=SC2317
 term_handler() {
-    if [ -n "${DISCORD_WEBHOOK_URL}" ] && [ -n "${DISCORD_PRE_SHUTDOWN_MESSAGE}" ]; then
-        su steam -c "/home/steam/server/discord.sh '${DISCORD_PRE_SHUTDOWN_MESSAGE}' in-progress" &
-    fi
+  DiscordMessage "${DISCORD_PRE_SHUTDOWN_MESSAGE}" "in-progress"
 
     if [ "${RCON_ENABLED,,}" = true ]; then
         rcon-cli save
@@ -37,7 +36,7 @@ wait "$killpid"
 
 mapfile -t backup_pids < <(pgrep backup)
 if [ "${#backup_pids[@]}" -ne 0 ]; then
-    echo "Waiting for backup to finish"
+    LogInfo "Waiting for backup to finish"
     for pid in "${backup_pids[@]}"; do
         tail --pid="$pid" -f 2>/dev/null
     done
@@ -45,7 +44,7 @@ fi
 
 mapfile -t restore_pids < <(pgrep restore)
 if [ "${#restore_pids[@]}" -ne 0 ]; then
-    echo "Waiting for restore to finish"
+    LogInfo "Waiting for restore to finish"
     for pid in "${restore_pids[@]}"; do
         tail --pid="$pid" -f 2>/dev/null
     done
