68aec6aa7a4f51d99942d3196e9234d33b53c5f8
Patrick <Patrick@networkglitch.com>
2024-02-10 18:06:55 -0500

    Updated feature branch with latest main changes

--- scripts/helper_functions.sh
+++ scripts/helper_functions.sh
@@ -78,3 +78,51 @@ get_player_count() {
     player_list=$(rcon-cli -c /home/steam/server/rcon.yaml "ShowPlayers")
     echo -n "${player_list}" | wc -l
 }
+#
+# Log Definitions
+#
+export LINE='\n'
+export RESET='\033[0m'       # Text Reset
+export WhiteText='\033[0;37m'        # White
+
+# Bold
+export RedBoldText='\033[1;31m'         # Red
+export GreenBoldText='\033[1;32m'       # Green
+export YellowBoldText='\033[1;33m'      # Yellow
+export CyanBoldText='\033[1;36m'        # Cyan
+
+LogInfo() {
+  Log "$1" "$WhiteText"
+}
+LogWarn() {
+  Log "$1" "$YellowBoldText"
+}
+LogError() {
+  Log "$1" "$RedBoldText"
+}
+LogSuccess() {
+  Log "$1" "$GreenBoldText"
+}
+LogAction() {
+  Log "$1" "$CyanBoldText" "****" "****"
+}
+Log() {
+  local message="$1"
+  local color="$2"
+  local prefix="$3"
+  local suffix="$4"
+  printf "$color%s$RESET$LINE" "$prefix$message$suffix"
+}
+
+# Send Discord Message
+# Level is optional variable defaulting to info
+DiscordMessage() {
+  local message="$1"
+  local level="$2"
+  if [ -n "$level" ]; then
+    level="info"
+  fi
+  if [ -n "${DISCORD_WEBHOOK_URL}" ]; then
+    /home/steam/server/discord.sh "$message" "$level" &
+  fi
+}
