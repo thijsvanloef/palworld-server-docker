6e759963b94cd1a924f603b39ff4583f7e8e3bed
Thijs van Loef <thijsvanloef@gmail.com>
2024-02-11 14:01:39 +0100

    set URL

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://palworld-server-docker.loef.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'thijsvanloef', // Usually your GitHub org/user name.
+  projectName: 'palworld-server-docker', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
