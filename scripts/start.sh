13aeffacf1f7c125e1b390166e02af185bffcb52
Carlos M. Martinez <Dashboy1998@gmail.com>
2024-02-13 12:46:49 -0500

    Added exit on bad crontab

--- scripts/start.sh
+++ scripts/start.sh
@@ -99,16 +99,19 @@ rm -f  "/home/steam/server/crontab"
 if [ "${BACKUP_ENABLED,,}" = true ]; then
     echo "BACKUP_ENABLED=${BACKUP_ENABLED,,}"
     echo "$BACKUP_CRON_EXPRESSION bash /usr/local/bin/backup" >> "/home/steam/server/crontab"
+    supercronic -quiet -test "/home/steam/server/crontab" || exit
 fi
 
 if [ "${AUTO_UPDATE_ENABLED,,}" = true ] && [ "${UPDATE_ON_BOOT}" = true ]; then
     echo "AUTO_UPDATE_ENABLED=${AUTO_UPDATE_ENABLED,,}"
     echo "$AUTO_UPDATE_CRON_EXPRESSION bash /usr/local/bin/update" >> "/home/steam/server/crontab"
+    supercronic -quiet -test "/home/steam/server/crontab" || exit
 fi
 
 if [ "${AUTO_REBOOT_ENABLED,,}" = true ] && [ "${RCON_ENABLED,,}" = true ]; then
     echo "AUTO_REBOOT_ENABLED=${AUTO_REBOOT_ENABLED,,}"
     echo "$AUTO_REBOOT_CRON_EXPRESSION bash /home/steam/server/auto_reboot.sh" >> "/home/steam/server/crontab"
+    supercronic -quiet -test "/home/steam/server/crontab" || exit
 fi
 
 if { [ "${AUTO_UPDATE_ENABLED,,}" = true ] && [ "${UPDATE_ON_BOOT,,}" = true ]; } || [ "${BACKUP_ENABLED,,}" = true ] || \
