a35af0191874f8cb875da19f5b11dcd005166fe3
Carlos M. Martinez <Dashboy1998@gmail.com>
2024-02-17 14:37:35 -0500

    Fixed update.sh issues from merge

--- scripts/update.sh
+++ scripts/update.sh
@@ -19,30 +19,18 @@ if [ "${RCON_ENABLED,,}" = false ]; then
     exit 1
 fi
 
-if [ "$CURRENT_MANIFEST" != "$TARGET_MANIFEST" ]; then
-    if [ "${RCON_ENABLED,,}" != true ]; then
-        echo "An update is available however auto updating without rcon is not supported"
-        if [ -n "${DISCORD_WEBHOOK_URL}" ]; then
-            /home/steam/server/discord.sh "An update is available however auto updating without rcon is not supported" "warn"
-        fi
-        exit 0
-    fi
+if [[ "${AUTO_UPDATE_WARN_MINUTES}" =~ ^[0-9]+$ ]]; then
+    DiscordMessage "Server will update in ${AUTO_UPDATE_WARN_MINUTES} minutes"
+fi
 
-    echo "An update is available"
-    if [[ "${AUTO_UPDATE_WARN_MINUTES}" =~ ^[0-9]+$ ]]; then
-        DiscordMessage "Server will update in ${AUTO_UPDATE_WARN_MINUTES} minutes"
-    fi
-    if countdown_message "${AUTO_UPDATE_WARN_MINUTES}" "Server_will_update"; then
-        LogAction "Updating the server from $CURRENT_MANIFEST to $TARGET_MANIFEST."
-        rm /palworld/steamapps/appmanifest_2394010.acf
+if countdown_message "${AUTO_UPDATE_WARN_MINUTES}" "Server_will_update"; then
+    LogAction "Updating the server from $CURRENT_MANIFEST to $TARGET_MANIFEST."
+    rm /palworld/steamapps/appmanifest_2394010.acf
 
-        backup
-        shutdown_server
-    elif [ -z "${AUTO_UPDATE_WARN_MINUTES}" ]; then
-        echo "Unable to auto update, the server is not empty and AUTO_UPDATE_WARN_MINUTES is empty."
-    else
-        echo "Unable to auto update, the server is not empty and AUTO_UPDATE_WARN_MINUTES is not an integer: ${AUTO_UPDATE_WARN_MINUTES}"
-    fi
+    backup
+    shutdown_server
+elif [ -z "${AUTO_UPDATE_WARN_MINUTES}" ]; then
+    LogWarn "Unable to auto update, the server is not empty and AUTO_UPDATE_WARN_MINUTES is empty."
 else
-    echo "The Server is up to date!"
+    LogWarn "Unable to auto update, the server is not empty and AUTO_UPDATE_WARN_MINUTES is not an integer: ${AUTO_UPDATE_WARN_MINUTES}"
 fi
