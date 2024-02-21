10f344db8db116aa8956d7b06f74acb4c0723a37
Carlos M. Martinez <Dashboy1998@gmail.com>
2024-02-21 16:53:16 -0500

    Fixed type

--- scripts/player_logging.sh
+++ scripts/player_logging.sh
@@ -16,7 +16,7 @@ old_player_list=( )
 while true; do
     mapfile -t server_pids < <(pgrep PalServer-Linux)
     if [ "${#server_pids[@]}" -ne 0 ]; then
-        # Player IDs are usally 7 digits however when a player joins for the first time for a given boot their ID is temporary 00000000 (8x zeros)
+        # Player IDs are usally 9 digits however when a player joins for the first time for a given boot their ID is temporary 00000000 (8x zeros)
         mapfile -t new_player_list < <(get_players_list | sed -E 's/,([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]|00000000),([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])/,\2/g' )
         # No players
         if [ "${#new_player_list[@]}" -gt 0 ] && [ "${#old_player_list[@]}" -gt 0 ]; then
