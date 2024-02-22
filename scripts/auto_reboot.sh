1c0ef7c63e30a6930837a9682179fa2d2f2d4b58
Carlos M. Martinez <Dashboy1998@gmail.com>
2024-02-21 23:24:06 -0500

    Minimize changes

--- scripts/auto_reboot.sh
+++ scripts/auto_reboot.sh
@@ -2,30 +2,29 @@
 # shellcheck source=scripts/helper_functions.sh
 source "/home/steam/server/helper_functions.sh"
 
-if [ "${RCON_ENABLED,,}" = true ]; then
-    if [ "${AUTO_REBOOT_EVEN_IF_PLAYERS_ONLINE,,}" != true ]; then
-        players_count=$(get_player_count)
-
-        if [ "$players_count" -gt 0 ]; then
-            LogWarn "There are ${players_count} players online. Skipping auto reboot."
-            exit 0
-        fi
-    fi
-
-    case "$(countdown_message "${AUTO_REBOOT_WARN_MINUTES}" "The_Server_will_reboot"; echo $?)" in
-        0 )
-            shutdown_server
-            ;;
-        1 )
-            LogError "Unable to auto reboot, the server is not empty and AUTO_REBOOT_WARN_MINUTES is empty"
-            exit 1
-            ;;
-        2 )
-            LogError "Unable to auto reboot, the server is not empty and AUTO_REBOOT_WARN_MINUTES is not an integer: ${AUTO_REBOOT_WARN_MINUTES}"
-            exit 1
-            ;;
-    esac
-else
+if [ "${RCON_ENABLED,,}" != true ]; then
     LogWarn "Unable to reboot. RCON is required."
     exit 1
 fi
+
+if [ "${AUTO_REBOOT_EVEN_IF_PLAYERS_ONLINE,,}" != true ]; then
+    players_count=$(get_player_count)
+    if [ "$players_count" -gt 0 ]; then
+        LogWarn "There are ${players_count} players online. Skipping auto reboot."
+        exit 0
+    fi
+fi
+
+case "$(countdown_message "${AUTO_REBOOT_WARN_MINUTES}" "The_Server_will_reboot"; echo $?)" in
+    0 )
+        shutdown_server
+        ;;
+    1 )
+        LogError "Unable to auto reboot, the server is not empty and AUTO_REBOOT_WARN_MINUTES is empty"
+        exit 1
+        ;;
+    2 )
+        LogError "Unable to auto reboot, the server is not empty and AUTO_REBOOT_WARN_MINUTES is not an integer: ${AUTO_REBOOT_WARN_MINUTES}"
+        exit 1
+        ;;
+esac
--- scripts/update.sh
+++ scripts/update.sh
@@ -6,39 +6,43 @@ source "/home/steam/server/helper_functions.sh"
 # shellcheck source=scripts/helper_install.sh
 source "/home/steam/server/helper_install.sh"
 
-# Check if Update is actually required
-if UpdateRequired; then
-    if [ "${UPDATE_ON_BOOT,,}" != true ]; then
-        LogWarn "An update is available however, UPDATE_ON_BOOT needs to be enabled for auto updating"
-        DiscordMessage "An update is available however, UPDATE_ON_BOOT needs to be enabled for auto updating" "warn"
-        exit 1
-    fi
-
-    if [ "${RCON_ENABLED,,}" != true ]; then
-        LogWarn "An update is available however auto updating without rcon is not supported"
-        DiscordMessage "An update is available however auto updating without rcon is not supported" "warn"
-        exit 1
-    fi
-
-    if [[ "${AUTO_UPDATE_WARN_MINUTES}" =~ ^[0-9]+$ ]]; then
-        DiscordMessage "Server will update in ${AUTO_UPDATE_WARN_MINUTES} minutes"
-    fi
-
-    case "$(countdown_message "${AUTO_UPDATE_WARN_MINUTES}" "Server_will_update"; echo $?)" in
-        0 )
-            LogAction "Updating the server from $CURRENT_MANIFEST to $TARGET_MANIFEST."
-            rm /palworld/steamapps/appmanifest_2394010.acf
-
-            backup
-            shutdown_server
-            ;;
-        1 )
-            LogWarn "Unable to auto update, the server is not empty and AUTO_UPDATE_WARN_MINUTES is empty."
-            exit 1
-            ;;
-        2 )
-            LogWarn "Unable to auto update, the server is not empty and AUTO_UPDATE_WARN_MINUTES is not an integer: ${AUTO_UPDATE_WARN_MINUTES}"
-            exit 1
-            ;;
-    esac
+UpdateRequired
+updateRequired=$?
+# Check if Update was actually required
+if [ "$updateRequired" != 0 ]; then
+  exit 0
 fi
+
+if [ "${UPDATE_ON_BOOT,,}" != true ]; then
+    LogWarn "An update is available however, UPDATE_ON_BOOT needs to be enabled for auto updating"
+    DiscordMessage "An update is available however, UPDATE_ON_BOOT needs to be enabled for auto updating" "warn"
+    exit 1
+fi
+
+if [ "${RCON_ENABLED,,}" != true ]; then
+    LogWarn "An update is available however auto updating without rcon is not supported"
+    DiscordMessage "An update is available however auto updating without rcon is not supported" "warn"
+    exit 1
+fi
+
+if [[ "${AUTO_UPDATE_WARN_MINUTES}" =~ ^[0-9]+$ ]]; then
+    DiscordMessage "Server will update in ${AUTO_UPDATE_WARN_MINUTES} minutes"
+fi
+
+case "$(countdown_message "${AUTO_UPDATE_WARN_MINUTES}" "Server_will_update"; echo $?)" in
+    0 )
+        LogAction "Updating the server from $CURRENT_MANIFEST to $TARGET_MANIFEST."
+        rm /palworld/steamapps/appmanifest_2394010.acf
+
+        backup
+        shutdown_server
+        ;;
+    1 )
+        LogWarn "Unable to auto update, the server is not empty and AUTO_UPDATE_WARN_MINUTES is empty."
+        exit 1
+        ;;
+    2 )
+        LogWarn "Unable to auto update, the server is not empty and AUTO_UPDATE_WARN_MINUTES is not an integer: ${AUTO_UPDATE_WARN_MINUTES}"
+        exit 1
+        ;;
+esac
