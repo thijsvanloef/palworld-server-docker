79ddad0fc10a6d42dbfcc542bf313553d8848936
Carlos M. Martinez <Dashboy1998@gmail.com>
2024-02-21 22:57:08 -0500

    Updated auto reboot

--- scripts/auto_reboot.sh
+++ scripts/auto_reboot.sh
@@ -2,31 +2,30 @@
 # shellcheck source=scripts/helper_functions.sh
 source "/home/steam/server/helper_functions.sh"
 
-if [ "${RCON_ENABLED,,}" != true ]; then
+if [ "${RCON_ENABLED,,}" = true ]; then
+    if [ "${AUTO_REBOOT_EVEN_IF_PLAYERS_ONLINE,,}" != true ]; then
+        players_count=$(get_player_count)
+
+        if [ "$players_count" -gt 0 ]; then
+            LogWarn "There are ${players_count} players online. Skipping auto reboot."
+            exit 0
+        fi
+    fi
+
+    case "$(countdown_message "${AUTO_REBOOT_WARN_MINUTES}" "The_Server_will_reboot"; echo $?)" in
+        0 )
+            shutdown_server
+            ;;
+        1 )
+            LogError "Unable to auto reboot, the server is not empty and AUTO_REBOOT_WARN_MINUTES is empty"
+            exit 1
+            ;;
+        2 )
+            LogError "Unable to auto reboot, the server is not empty and AUTO_REBOOT_WARN_MINUTES is not an integer: ${AUTO_REBOOT_WARN_MINUTES}"
+            exit 1
+            ;;
+    esac
+else
     LogWarn "Unable to reboot. RCON is required."
-    exit 0
+    exit 1
 fi
-
-if [ -z "${AUTO_REBOOT_WARN_MINUTES}" ]; then
-    LogError "Unable to auto reboot, AUTO_REBOOT_WARN_MINUTES is empty."
-    exit 0
-fi
-
-if [ "${AUTO_REBOOT_EVEN_IF_PLAYERS_ONLINE,,}" != true ]; then
-  players_count=$(get_player_count)
-  if [ "$players_count" -gt 0 ]; then
-    LogWarn "There are ${players_count} players online. Skipping auto reboot."
-    exit 0
-  fi
-fi
-
-if [[ "${AUTO_REBOOT_WARN_MINUTES}" =~ ^[0-9]+$ ]]; then
-    for ((i = "${AUTO_REBOOT_WARN_MINUTES}" ; i > 0 ; i--)); do
-        broadcast_command "The Server will reboot in ${i} minutes"
-        sleep "1m"
-    done
-    shutdown_server
-    exit 0
-fi
-
-LogError "Unable to auto reboot, AUTO_REBOOT_WARN_MINUTES is not an integer: ${AUTO_REBOOT_WARN_MINUTES}"
