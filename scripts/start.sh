27833a9dfcf00b042ec64b5d4e95f7b334fece38
Robert Bost <rbost@redhat.com>
2024-02-26 21:38:01 -0700

    Include RCON Port CLI flag when enabled

--- scripts/start.sh
+++ scripts/start.sh
@@ -81,6 +81,10 @@ if [ "${MULTITHREADING,,}" = true ]; then
     STARTCOMMAND+=("-useperfthreads" "-NoAsyncLoadingThread" "-UseMultithreadForDS")
 fi
 
+if [ "${RCON_ENABLED,,}" = true ]; then
+    STARTCOMMAND+=("-rconport=${RCON_PORT}")
+fi
+
 if [ "${DISABLE_GENERATE_SETTINGS,,}" = true ]; then
   LogAction "GENERATING CONFIG"
   LogWarn "Env vars will not be applied due to DISABLE_GENERATE_SETTINGS being set to TRUE!"
