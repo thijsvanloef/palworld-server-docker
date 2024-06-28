fa99c92715ec1319cf15cb7a2970512cb3cd2f0c
muscle <muscle@a-b-c-d.com>
2024-06-28 11:48:49 +0900

    fix REST API a little. (docs, indent, return value)

--- scripts/rest_api.sh
+++ scripts/rest_api.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-SCRIPT=$(basename "${0}")
-ORGPATH=$(readlink -fn "${0}")
-ORGDIR=$(dirname "${ORGPATH}")
+SCRIPT_DIR=${SCRIPT_DIR:-$(dirname "$(readlink -fn "${0}")")}
 #shellcheck source=scripts/helper_functions.sh
-source "${ORGDIR}/helper_functions.sh"
+source "${SCRIPT_DIR}/helper_functions.sh"
+
+SCRIPT=$(basename "${0}")
+
 help="-h|--help"
 if [ $# -lt 1 ] || [[ ${1} =~ ${help} ]]; then
-cat << EOF
+    cat << EOF
 Usage: ${SCRIPT} <api> [options]
 api:
   announce <json> ... announce message.
@@ -25,68 +26,68 @@ options:
   -               ... json from stdin.
   -h, --help      ... help.
 EOF
-  exit 1
+    exit 1
 fi
 
 if [ ! "${REST_API_ENABLED,,}" = true ]; then
-  echo "ERROR: REST_API_ENABLED=False"
-  exit 1
+    echo "ERROR: REST_API_ENABLED=False"
+    exit 1
 fi
 
 api="${1}"
 json="${2}"
 api_required_json="announce|ban|kick|shutdown|unban"
 if [[ ${api} =~ ${api_required_json} ]]; then
-  if [ $# -lt 2 ]; then
-    echo "input json required."
-    exit 1
-  fi
-  if [ "${json}" = "-" ]; then
-    json="$(cat -)"
-  elif [[ ! ${json} =~ ^\{ ]]; then
-    usage="Usage: ${SCRIPT} ${api}"
-    case ${api} in
-    "announce")
-      if [[ ${json} =~ ${help} ]]; then
-        echo "${usage} <message>"
+    if [ $# -lt 2 ]; then
+        echo "input json required."
         exit 1
-      fi
-      json="{\"message\":\"${2}\"}"
-      ;;
-    "ban")
-      if [[ ${json} =~ ${help} ]]; then
-        echo "${usage} <steam_00000000000000000> [message]"
-        exit 1
-      fi
-      msg=${3:-You are banned.}
-      json="{\"userid\":\"${2}\",\"message\":\"${msg}\"}"
-      ;;
-    "kick")
-      if [[ ${json} =~ ${help} ]]; then
-        echo "${usage} <steam_00000000000000000> [message]"
-        exit 1
-      fi
-      msg=${3:-You are kicked.}
-      json="{\"userid\":\"${2}\",\"message\":\"${msg}\"}"
-      ;;
-    "shutdown")
-      if [[ ${json} =~ ${help} ]]; then
-        echo "${usage} <sec> [message]"
-        exit 1
-      fi
-      sec=${2}
-      msg=${3:-Server will shutdown in ${sec} sec.}
-      json="{\"waittime\":${sec},\"message\":\"${msg}\"}"
-      ;;
-    "unban")
-      if [[ ${json} =~ ${help} ]]; then
-        echo "${usage} <steam_00000000000000000>"
-        exit 1
-      fi
-      json="{\"userid\":\"${2}\"}"
-      ;;
-    esac
-  fi
+    fi
+    if [ "${json}" = "-" ]; then
+        json="$(cat -)"
+    elif [[ ! ${json} =~ ^\{ ]]; then
+        usage="Usage: ${SCRIPT} ${api}"
+        case ${api} in
+        "announce")
+            if [[ ${json} =~ ${help} ]]; then
+                echo "${usage} <message>"
+                exit 1
+            fi
+            json="{\"message\":\"${2}\"}"
+            ;;
+        "ban")
+            if [[ ${json} =~ ${help} ]]; then
+                echo "${usage} <steam_00000000000000000> [message]"
+                exit 1
+            fi
+            msg=${3:-You are banned.}
+            json="{\"userid\":\"${2}\",\"message\":\"${msg}\"}"
+            ;;
+        "kick")
+            if [[ ${json} =~ ${help} ]]; then
+                echo "${usage} <steam_00000000000000000> [message]"
+                exit 1
+            fi
+            msg=${3:-You are kicked.}
+            json="{\"userid\":\"${2}\",\"message\":\"${msg}\"}"
+            ;;
+        "shutdown")
+            if [[ ${json} =~ ${help} ]]; then
+                echo "${usage} <sec> [message]"
+                exit 1
+            fi
+            sec=${2}
+            msg=${3:-Server will shutdown in ${sec} sec.}
+            json="{\"waittime\":${sec},\"message\":\"${msg}\"}"
+            ;;
+        "unban")
+            if [[ ${json} =~ ${help} ]]; then
+                echo "${usage} <steam_00000000000000000>"
+                exit 1
+            fi
+            json="{\"userid\":\"${2}\"}"
+            ;;
+        esac
+    fi
 fi
 
 REST_API "${api}" "${json}" && echo ""
