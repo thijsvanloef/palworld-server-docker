163077f4d46c107c74bafaa70c9f47f2a71eab93
Tim Ipema <tim@indifferentbroccoli.com>
2026-07-27 09:49:31 -0500

    Skip Throwaway Server Boot when DISABLE_GENERATE_SETTINGS=true

--- scripts/start.sh
+++ scripts/start.sh
@@ -114,15 +114,7 @@ if [ "${DISABLE_GENERATE_SETTINGS,,}" = true ]; then
   # shellcheck disable=SC2143
   if [ ! "$(grep -s '[^[:space:]]' /palworld/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini)" ]; then
       LogAction "GENERATING CONFIG"
-      # Server will generate all ini files after first run.
-      if [ "$architecture" == "arm64" ]; then
-          timeout --preserve-status 15s ./PalServer-arm64.sh 1> /dev/null
-      else
-          timeout --preserve-status 15s ./PalServer.sh 1> /dev/null
-      fi
-
-      # Wait for shutdown
-      sleep 5
+      mkdir -p /palworld/Pal/Saved/Config/LinuxServer || exit
       cp /palworld/DefaultPalWorldSettings.ini /palworld/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini
   fi
 else
